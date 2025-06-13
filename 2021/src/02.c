304a5886990ec911b4fa19f5ff26ba50a239b954
Benjamin Andre <benjaminandre23@gmail.com>
2025-06-13 21:53:46 +0200

    aoc21(day02): completed 02 parts 1 + 2

--- 2021/src/02.c
+++ 2021/src/02.c
@@ -1,15 +1,32 @@
 #include <stdio.h>
-#include <stdlib.h>
 #include <string.h>
 #include "aoc.h"
 
 void solve_02(char **lines, int line_count) {
-    (void)lines;      // Kachow !
-    (void)line_count; // This shit is just to suppress warning for now
-    
-    int part1 = 0, part2 = 0;
-    
-    // TODO: Implement solution
-    
+    int horizontal = 0;
+    int depth = 0;
+    int new_depth = 0;
+
+    for (int i = 0; i < line_count; i++) {
+        char direction[10];
+        int number;
+
+        if (sscanf(lines[i], "%s %d", direction, &number) == 2) {
+            if (strcmp(direction, "forward") == 0) {
+                horizontal += number;
+                new_depth += depth * number;
+            }
+            else if (strcmp(direction, "down") == 0) {
+                depth += number;
+            }
+            else {
+                depth -= number;
+            }
+        }
+    }
+
+    int part1 = horizontal * depth;
+    int part2 = horizontal * new_depth;
+
     printf("Part 1: %d\nPart 2: %d\n", part1, part2);
 }
