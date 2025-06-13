859577ea54d1c2cc3f32e4edd43d9d586d930e81
Benjamin Andre <benjaminandre23@gmail.com>
2025-06-13 11:10:29 +0200

    aoc21(day01): completed 01 parts 1 + 2

--- 2021/src/01.c
+++ 2021/src/01.c
@@ -4,12 +4,33 @@
 #include "aoc.h"
 
 void solve_01(char **lines, int line_count) {
-    (void)lines;      // Kachow !
-    (void)line_count; // This shit is just to suppress warning for now
-    
-    int part1 = 0, part2 = 0;
-    
-    // TODO: Implement solution
-    
+    int *input = malloc(line_count * sizeof(int));
+    if (!input) {
+        printf("Kachow ! Memory allocation failed\n");
+        return;
+    }
+
+    for (int i = 0; i < line_count; i++) {
+        input[i] = atoi(lines[i]);
+    }
+
+    int count = 0;
+    for (int i = 1; i < line_count; i++) {
+        if (input[i] > input[i - 1])
+            count++;
+    }
+    int part1 = count;
+
+    count = 0;
+    for (int i = 3; i < line_count; i++) {
+        int prev = input[i - 3] + input[i - 2] + input[i - 1];
+        int cur = input[i - 2] + input[i - 1] + input[i];
+        if (cur > prev)
+            count++;
+    }
+    int part2 = count;
+
     printf("Part 1: %d\nPart 2: %d\n", part1, part2);
+
+    free(input);
 }
