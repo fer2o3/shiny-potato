250d7f29e2b3f82aaf744bd5f671cc07c5b0f732
Benjamin Andre <benjaminandre23@gmail.com>
2025-06-15 09:04:40 +0200

    aoc21(day05): completed 05 parts 1 + 2

--- 2021/src/05.c
+++ 2021/src/05.c
@@ -1,15 +1,48 @@
 #include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
 #include "aoc.h"
 
+#define GRID_SIZE 1000
+
+struct line {
+    int x1, y1, x2, y2;
+};
+
+void plot_line(struct line *line, int grid[GRID_SIZE][GRID_SIZE]) {
+    int x = line->x1, y = line->y1;
+    int dx = (x < line->x2) ? 1 : (x > line->x2) ? -1 : 0;
+    int dy = (y < line->y2) ? 1 : (y > line->y2) ? -1 : 0;
+
+    do {
+        grid[x][y]++;
+        x += dx;
+        y += dy;
+    } while (x != line->x2 || y != line->y2);
+    grid[line->x2][line->y2]++;
+}
+
+int count_intersections(int grid[GRID_SIZE][GRID_SIZE]) {
+    int count = 0;
+    for (int i = 0; i < GRID_SIZE; i++)
+        for (int j = 0; j < GRID_SIZE; j++)
+            if (grid[i][j] > 1) count++;
+    return count;
+}
+
 void solve_05(char **lines, int line_count) {
-    (void)lines;      // Kachow !
-    (void)line_count; // This shit is just to suppress warning for now
-    
-    int part1 = 0, part2 = 0;
-    
-    // TODO: Implement solution
-    
-    printf("Part 1: %d\nPart 2: %d\n", part1, part2);
+    static int grid1[GRID_SIZE][GRID_SIZE] = {0};
+    static int grid2[GRID_SIZE][GRID_SIZE] = {0};
+
+    for (int i = 0; i < line_count; i++) {
+        struct line l;
+        sscanf(lines[i], "%d,%d -> %d,%d", &l.x1, &l.y1, &l.x2, &l.y2);
+
+        if (l.x1 == l.x2 || l.y1 == l.y2)
+            plot_line(&l, grid1);
+
+        plot_line(&l, grid2);
+    }
+
+    printf("Part 1: %d\nPart 2: %d\n", 
+           count_intersections(grid1), 
+           count_intersections(grid2));
 }
