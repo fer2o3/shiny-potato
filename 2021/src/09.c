98ac9d0238df8d24fa1082469ad1e9d997f165d8
Benjamin Andre <benjaminandre23@gmail.com>
2025-06-24 13:22:27 +0200

    aoc21(day09): completed part 1

--- 2021/src/09.c
+++ 2021/src/09.c
@@ -3,13 +3,41 @@
 #include <string.h>
 #include "aoc.h"
 
+int is_low_point(char grid[][200], int rows, int cols, int r, int c) {
+    int height = grid[r][c] - '0';
+
+    int directions[4][2] = {{-1,0}, {1,0}, {0,-1}, {0,1}};
+
+    for (int i = 0; i < 4; i++) {
+        int nr = r + directions[i][0];
+        int nc = c + directions[i][1];
+
+        if (nr < 0 || nr >= rows || nc < 0 || nc >= cols) continue;
+
+        if (grid[nr][nc] - '0' <= height) return 0;
+    }
+
+    return 1;
+}
+
 void solve_09(char **lines, int line_count) {
-    (void)lines;      // Kachow !
-    (void)line_count; // This shit is just to suppress warning for now
-    
+    char grid[200][200];
+    int rows = line_count;
+    int cols = strlen(lines[0]);
+
+    for (int i = 0; i < rows; i++) {
+        strcpy(grid[i], lines[i]);
+    }
+
     int part1 = 0, part2 = 0;
-    
-    // TODO: Implement solution
+
+    for (int r = 0; r < rows; r++) {
+        for (int c = 0; c < cols; c++) {
+            if (is_low_point(grid, rows, cols, r, c)) {
+                part1 += (grid[r][c] - '0') + 1;
+            }
+        }
+    }
     
     printf("Part 1: %d\nPart 2: %d\n", part1, part2);
 }
