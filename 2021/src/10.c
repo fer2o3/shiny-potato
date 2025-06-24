0ec7959e2dda359271e6c109fe34e4397bff9a11
Benjamin Andre <benjaminandre23@gmail.com>
2025-06-24 18:40:32 +0200

    aoc21(day10): completed part 2

--- 2021/src/10.c
+++ 2021/src/10.c
@@ -3,8 +3,8 @@
 #include <string.h>
 #include "aoc.h"
 
-int get_score(char illegal) {
-    switch (illegal) {
+long get_score(char c) {
+    switch (c) {
         case ')': return 3;
         case ']': return 57;
         case '}': return 1197;
@@ -13,8 +13,18 @@ int get_score(char illegal) {
     }
 }
 
-char match_char(char open) {
-    switch (open) {
+long get_value(char c) {
+    switch (c) {
+        case '(': return 1;
+        case '[': return 2;
+        case '{': return 3;
+        case '<': return 4;
+        default: return 0;
+    }
+}
+
+char match_char(char c) {
+    switch (c) {
         case '(': return ')';
         case '[': return ']';
         case '{': return '}';
@@ -23,11 +33,11 @@ char match_char(char open) {
     }
 }
 
-int check_line(char *line) {
+long check_line(char *line) {
     char stack[200];
-    int top = -1;
-    
-    for (int i = 0; line[i]; i++) {
+    long top = -1;
+
+    for (long i = 0; line[i]; i++) {
         char c = line[i];
 
         if (c == '(' || c == '[' || c == '{' || c == '<') {
@@ -48,16 +58,39 @@ int check_line(char *line) {
         }
     }
 
-    return 0;
+    long score = 0;
+    for (long i = top; i >= 0; i--) {
+        score *= 5;
+        score += get_value(stack[i]);
+    }
+
+    return -score;
+}
+
+void sort(long *arr, long len) {
+    for (long i = 1; i < len; ++i) {
+        long key = arr[i], j = i - 1;
+        while (j >= 0 && arr[j] > key) {
+            arr[j + 1] = arr[j];
+            j--;
+        }
+        arr[j + 1] = key;
+    }
 }
 
 void solve_10(char **lines, int line_count) {
-    int part1 = 0, part2 = 0;
+    long part1 = 0, part2 = 0;
+    long arr[200];
+    long len = 0;
 
-    for (int i = 0; i < line_count; i++) {
-        int score = check_line(lines[i]);
-        part1 += score;
+    for (long i = 0; i < line_count; i++) {
+        long score = check_line(lines[i]);
+        if (score >= 0) part1 += score;
+        else arr[len++] = -score;
     }
+    
+    sort(arr, len);
+    part2 = arr[len/2];
 
-    printf("Part 1: %d\nPart 2: %d\n", part1, part2);
+    printf("Part 1: %ld\nPart 2: %ld\n", part1, part2);
 }
