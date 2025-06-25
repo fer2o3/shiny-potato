ebf3e25b46452356819f65eafec474f48a8b33b0
Benjamin Andre <benjaminandre23@gmail.com>
2025-06-25 11:10:05 +0200

    feat(2021-11): completed part 1

--- 2021/src/11.c
+++ 2021/src/11.c
@@ -3,13 +3,60 @@
 #include <string.h>
 #include "aoc.h"
 
+int flash(int grid[][10], int r, int c) {
+    int flashes = 1;
+    grid[r][c] = -9999;
+    int dirs[8][2] = {
+        {-1,0}, {1,0}, {0,-1}, {0,1}, 
+        {1,1}, {-1,-1}, {1,-1}, {-1,1}
+    };
+
+    for (int i = 0; i < 8; i++) {
+        int nr = r + dirs[i][0], nc = c + dirs[i][1];
+        if (nr >= 0 && nr < 10 && nc >= 0 && nc < 10) {
+            grid[nr][nc]++;
+            if (grid[nr][nc] > 9) flashes += flash(grid, nr, nc);
+        }
+    }
+
+    return flashes;
+}
+
+int step(int grid[][10]) {
+    // First
+    int flashes = 0;
+    for (int i = 0; i < 10; i++)
+        for (int j = 0; j < 10; j++)
+            grid[i][j]++;
+
+    // Then
+    for (int i = 0; i < 10; i++)
+        for (int j = 0; j < 10; j++)
+            if (grid[i][j] > 9)
+                flashes += flash(grid, i, j);
+
+    // Finally
+    for (int i = 0; i < 10; i++)
+        for (int j = 0; j < 10; j++)
+            if (grid[i][j] < 0)
+                grid[i][j] = 0;
+
+    return flashes;
+}
+
 void solve_11(char **lines, int line_count) {
-    (void)lines;      // Kachow !
     (void)line_count; // This shit is just to suppress warning for now
-    
+
+    int grid[10][10];
+    int rows = 10, cols = 10;
+    for (int i = 0; i < rows; i++)
+        for (int j = 0; j < cols; j++)
+            grid[i][j] = lines[i][j] - '0';
+
     int part1 = 0, part2 = 0;
-    
-    // TODO: Implement solution
-    
+    int iter = 100;
+    while (iter--)
+        part1 += step(grid);
+
     printf("Part 1: %d\nPart 2: %d\n", part1, part2);
 }
