c9adb6f09af6cdaf52da5b5dddf819ef702b4d88
Benjamin Andre <benjaminandre23@gmail.com>
2025-06-26 15:39:57 +0200

    feat(2021-14): completed parts 1 + 2

--- 2021/src/14.c
+++ 2021/src/14.c
@@ -1,15 +1,76 @@
+#include "aoc.h"
+#include <limits.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include "aoc.h"
+
+char rules[26][26];
+long long pairs[26][26];
+
+long long score() {
+    long long counts[26] = {0};
+
+    for (int i = 0; i < 26; i++) {
+        for (int j = 0; j < 26; j++) {
+            counts[i] += pairs[i][j];
+        }
+    }
+
+    long long min_count = LLONG_MAX, max_count = 0;
+    for (int i = 0; i < 26; i++) {
+        if (counts[i] > 0) {
+            if (counts[i] < min_count)
+                min_count = counts[i];
+            if (counts[i] > max_count)
+                max_count = counts[i];
+        }
+    }
+    return max_count - min_count;
+}
 
 void solve_14(char **lines, int line_count) {
-    (void)lines;      // Kachow !
-    (void)line_count; // This shit is just to suppress warning for now
-    
-    int part1 = 0, part2 = 0;
-    
-    // TODO: Implement solution
-    
-    printf("Part 1: %d\nPart 2: %d\n", part1, part2);
+    memset(rules, 0, sizeof(rules));
+    memset(pairs, 0, sizeof(pairs));
+
+    char *template = lines[0];
+    int len = strlen(template);
+
+    for (int i = 0; i < len - 1; i++) {
+        pairs[template[i] - 'A'][template[i + 1] - 'A']++;
+    }
+
+    for (int i = 2; i < line_count; i++) {
+        char pair[3], insert;
+        sscanf(lines[i], "%2s -> %c", pair, &insert);
+        rules[pair[0] - 'A'][pair[1] - 'A'] = insert - 'A';
+    }
+
+    long long part1 = 0;
+
+    for (int step = 0; step < 40; step++) {
+        long long new_counts[26][26] = {0};
+
+        for (int i = 0; i < 26; i++) {
+            for (int j = 0; j < 26; j++) {
+                if (pairs[i][j] > 0 && rules[i][j] >= 0) {
+                    int c = rules[i][j];
+                    new_counts[i][c] += pairs[i][j];
+                    new_counts[c][j] += pairs[i][j];
+                }
+            }
+        }
+
+        memcpy(pairs, new_counts, sizeof(pairs));
+
+        if (step == 9) {
+            pairs[template[len - 1] - 'A'][0]++;
+            part1 = score();
+            pairs[template[len - 1] - 'A'][0]--;
+        }
+    }
+
+    pairs[template[len - 1] - 'A'][0]++;
+    long long part2 = score();
+
+    printf("Part 1: %lld\nPart 2: %lld\n", part1, part2);
 }
