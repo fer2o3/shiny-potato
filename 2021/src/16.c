91c55b8c9d5d32497af79adec8aec45da020b13d
Benjamin Andre <benjaminandre23@gmail.com>
2025-07-01 09:14:28 +0200

    feat(2021-16): completed part 2

--- 2021/src/16.c
+++ 2021/src/16.c
@@ -1,4 +1,5 @@
 #include "aoc.h"
+#include <limits.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
@@ -15,40 +16,75 @@ int get_bits(const char *hex, int pos, int len) {
     return val;
 }
 
-// This was definitely not the cleanest way to solve this shit
-int parse_packet(const char *hex, int pos, int *version_sum) {
-    int version = get_bits(hex, pos, 3);
-    int type = get_bits(hex, pos + 3, 3);
+unsigned long long parse_packet(const char *hex, int *pos, int *version_sum) {
+    int version = get_bits(hex, *pos, 3);
+    int type = get_bits(hex, *pos + 3, 3);
     *version_sum += version;
-    pos += 6;
+    *pos += 6;
 
     if (type == 4) {
-        while (get_bits(hex, pos, 1))
-            pos += 5;
-        return pos + 5;
-    } else {
-        int length_type = get_bits(hex, pos++, 1);
-        if (length_type == 0) {
-            int sub_len = get_bits(hex, pos, 15);
-            pos += 15;
-            int end = pos + sub_len;
-            while (pos < end)
-                pos = parse_packet(hex, pos, version_sum);
-        } else {
-            int sub_count = get_bits(hex, pos, 11);
-            pos += 11;
-            for (int i = 0; i < sub_count; i++)
-                pos = parse_packet(hex, pos, version_sum);
+        unsigned long long val = 0;
+        while (get_bits(hex, *pos, 1)) {
+            val = (val << 4) | get_bits(hex, *pos + 1, 4);
+            *pos += 5;
         }
-        return pos;
+        val = (val << 4) | get_bits(hex, *pos + 1, 4);
+        *pos += 5;
+        return val;
     }
+
+    unsigned long long vals[1000];
+    int count = 0;
+
+    if (get_bits(hex, (*pos)++, 1) == 0) {
+        int end = *pos + 15 + get_bits(hex, *pos, 15);
+        *pos += 15;
+        while (*pos < end)
+            vals[count++] = parse_packet(hex, pos, version_sum);
+    } else {
+        int sub_count = get_bits(hex, *pos, 11);
+        *pos += 11;
+        for (int i = 0; i < sub_count; i++)
+            vals[count++] = parse_packet(hex, pos, version_sum);
+    }
+
+    unsigned long long result = vals[0];
+    switch (type) {
+    case 0:
+        for (int i = 1; i < count; i++)
+            result += vals[i];
+        break;
+    case 1:
+        for (int i = 1; i < count; i++)
+            result *= vals[i];
+        break;
+    case 2:
+        for (int i = 1; i < count; i++)
+            if (vals[i] < result)
+                result = vals[i];
+        break;
+    case 3:
+        for (int i = 1; i < count; i++)
+            if (vals[i] > result)
+                result = vals[i];
+        break;
+    case 5:
+        result = vals[0] > vals[1];
+        break;
+    case 6:
+        result = vals[0] < vals[1];
+        break;
+    case 7:
+        result = vals[0] == vals[1];
+        break;
+    }
+    return result;
 }
 
 void solve_16(char **lines, int line_count) {
+    (void)line_count; // This shit is just to suppress warning for now
     char *hex = lines[0];
-
-    int part1 = 0;
-    parse_packet(hex, 0, &part1);
-
-    printf("Part 1: %d\nPart 2: %d\n", part1, 0);
+    int part1 = 0, pos = 0;
+    unsigned long long part2 = parse_packet(hex, &pos, &part1);
+    printf("Part 1: %d\nPart 2: %llu\n", part1, part2);
 }
