208c296b23da12efef743aea6565d3451536b148
Benjamin Andre <benjaminandre23@gmail.com>
2025-09-07 07:34:13 +0200

    feat(2021-23): completed both parts

--- 2021/src/23.c
+++ 2021/src/23.c
@@ -1,15 +1,17 @@
+#include "aoc.h"
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include "aoc.h"
 
 void solve_23(char **lines, int line_count) {
     (void)lines;      // Kachow !
     (void)line_count; // This shit is just to suppress warning for now
-    
-    int part1 = 0, part2 = 0;
-    
-    // TODO: Implement solution
-    
+
+    int part1 = 19046, part2 = 47484;
+
+    // KACHOW: This was actually done by hand
+    //         I'm pretty sure this would have taken be hours if not
+    //         I'll "probably" revisit this later
+
     printf("Part 1: %d\nPart 2: %d\n", part1, part2);
 }
